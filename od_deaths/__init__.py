59cfdf6c5865212298324ee0aa0d8f135885a8f2
Mark Butler <mrkcbutler@gmail.com>
2022-06-22 18:24:52 -0500

    Add a basic plotly figure to the app

--- od_deaths/__init__.py
+++ od_deaths/__init__.py
@@ -3,6 +3,11 @@ from pathlib import Path
 
 from flask import Flask, render_template
 
+# Included for exploratory purposes.  To be removed later.
+import json
+import plotly
+import plotly.express as px
+
 
 def create_app():
     """Generate an instance of the app."""
@@ -16,6 +21,8 @@ def create_app():
 
     @app.route('/')
     def index():
-        return render_template('app.html')
+        fig = px.bar(x=['a', 'b', 'c'], y=[1, 3, 2])
+        test_chart = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+        return render_template('app.html', test_chart=test_chart)
 
     return app
