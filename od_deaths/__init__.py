adc266186cba83aec501e23fd0857330734b4687
Mark Butler <mrkcbutler@gmail.com>
2022-06-25 10:22:21 -0500

    Use fetch API to get plotly figure from server

--- od_deaths/__init__.py
+++ od_deaths/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template
 
 # Included for exploratory purposes.  To be removed later.
 import json
+from flask import make_response
 import plotly
 import plotly.express as px
 
@@ -21,8 +22,15 @@ def create_app():
 
     @app.route('/')
     def index():
+        return render_template('app.html')
+
+    @app.route('/test-chart')
+    def get_test_chart():
         fig = px.bar(x=['a', 'b', 'c'], y=[1, 3, 2])
-        test_chart = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-        return render_template('app.html', test_chart=test_chart)
+        response = make_response(
+            json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+        )
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
     return app
