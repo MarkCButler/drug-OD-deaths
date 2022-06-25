7691124fbacfe74df380d7d6afd4c96ea1f10699
Mark Butler <mrkcbutler@gmail.com>
2022-06-25 14:56:23 -0500

    Use async/await in fetching figure from server

--- front-end/src/js/plots.js
+++ front-end/src/js/plots.js
@@ -11,27 +11,34 @@ class HTTPError extends Error {
 }
 
 
-function addPlot(divElem, url, plotName) {
-  fetch(url)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new HTTPError(`status code ${response.status}`);
-        }
-      })
-      .then((plotJSON) => {
-        const config = {responsive: true};
-        const data = plotJSON['data'];
-        const layout = plotJSON['layout'];
-        Plotly.newPlot(testPlot, data, layout, config);
-      })
-      .catch((error) => {
-        testPlot.classList.add('h3', 'text-warning');
-        testPlot.innerHTML = `<p>Failed to fetch or generate ${plotName}.</p>` +
-                             `<p>${error}</p>`;
-      });
+async function addPlot(divElem, url, plotName) {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const plotJSON = await response.json();
+      const config = {responsive: true};
+      const data = plotJSON['data'];
+      const layout = plotJSON['layout'];
+      Plotly.newPlot(divElem, data, layout, config);
+    } else {
+      throw new HTTPError(`status code ${response.status}`);
+    }
+  } catch (error) {
+    displayPlotError(error, divElem, plotName);
+  }
+}
+
+
+function displayPlotError(error, divElem, plotName) {
+  if (!divElem.classList.contains('h3')) {
+    divElem.classList.add('h3');
+  }
+  if (!divElem.classList.contains('text-warning')) {
+    divElem.classList.add('text-warning');
+  }
+  divElem.innerHTML = `<p>Failed to fetch or generate ${plotName}.</p>` +
+                      `<p>${error}</p>`;
 }
 
 const testPlot = document.getElementById('test-plot');
-addPlot(testPlot, '/test-plot', 'test-plot');
+void addPlot(testPlot, '/test-plot', 'test-plot');
