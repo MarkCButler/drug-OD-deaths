2c9fcdea9c39e765f249bb1faf4ab90ca3a38783
Mark Butler <mrkcbutler@gmail.com>
2022-10-31 23:17:17 -0500

    Add handlers to resize graphs after bootstrap events

--- front-end/src/js/plots.js
+++ front-end/src/js/plots.js
@@ -49,3 +49,47 @@ for (const plotID of plotIDs) {
   const testPlot = document.getElementById(plotID);
   void addPlot(testPlot, `/plots/${plotID}`, plotID);
 }
+
+
+// Plotly objects do not natively understand bootstrap events, so we need to add
+// handlers to resize plots after to certain bootstrap events:
+//   1. After the sidebar has been hidden or shown, which corresponds to events
+//   hidden.bs.collapse, shown.bs.collapse on the sidebar.
+//   2. After a tab pane has been hidden or shown, which corresponds to events
+//   hidden.bs.tab, shown.bs.tab on the tab pane.
+// When the sidebar or tab is visible, it will have the CSS class show.
+const tabPaneIDs = ['summary', 'map', 'time-dev', 'data', 'notes'];
+const tabPanes = tabPaneIDs.map((id) => document.getElementById(id));
+const sidebar = document.getElementById('sidebar');
+
+
+function resizeTabPanePlots(tabPane) {
+  const plots = tabPane.querySelectorAll('.od-deaths-plot');
+  for (const plot of plots) {
+    Plotly.Plots.resize(plot);
+  }
+}
+
+
+function refreshTabPanes() {
+  for (const tabPane of tabPanes) {
+    // If a tabPane is visible, resize the plots contained in it.
+    if (tabPane.classList.contains('show')) {
+      resizeTabPanePlots(tabPane);
+      break;
+    }
+  }
+}
+
+// Add event listeners to resize plots after the sidebar is toggled.
+['hidden.bs.collapse', 'shown.bs.collapse'].forEach(
+    (eventType) => sidebar.addEventListener(eventType, refreshTabPanes)
+);
+
+// Add event listener to resize the plots of a tab that has just been made
+// visible.
+sidebar.addEventListener('shown.bs.tab', (event) => {
+  const idSelector = event.target.dataset.bsTarget;
+  const tabPane = document.querySelector(idSelector);
+  resizeTabPanePlots(tabPane);
+});
