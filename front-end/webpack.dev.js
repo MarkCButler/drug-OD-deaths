3368f5c2093913c7aa04ffc0649d7b359dbeca7d
Mark Butler <mrkcbutler@gmail.com>
2022-11-05 11:21:08 -0500

    Add source-map processing to webpack build

--- front-end/webpack.dev.js
+++ front-end/webpack.dev.js
@@ -3,4 +3,14 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
+  devtool: 'eval',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+    ],
+  },
 });
